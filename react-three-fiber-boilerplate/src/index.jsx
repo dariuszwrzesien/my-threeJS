6892f0fa51aa23f5e143a5ed5a4aa46f201a7408
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2025-07-10 10:09:05 +0200

    Update index.html title and refactor index.jsx rendering method

--- react-three-fiber-boilerplate/src/index.jsx
+++ react-three-fiber-boilerplate/src/index.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// )
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
